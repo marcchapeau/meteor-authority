42a877993fad0f805360c7982d69649e460111a8
Marc Chapeau <marcchapeau@gmail.com>
2018-05-21 19:23:16 +0200

    Bumped version number to 1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chap:authority',
-  version: '1.0.2',
+  version: '1.0.3',
   summary: 'Roles and permissions package for Meteor',
   git: 'https://github.com/marcchapeau/meteor-authority.git',
   documentation: 'README.md'
